3d1f8b4b118c301f8ba9e87cf8c9a59445e3edb5
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-07-12 21:51:42 -0500

    completed add movie form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Movie from './components/Movie';
 import MovieHeader from './components/MovieHeader';
 
 import EditMovieForm from './components/EditMovieForm';
+import AddMovieForm from "./components/AddMovieForm";
 import FavoriteMovieList from './components/FavoriteMovieList';
 
 import axios from 'axios';
@@ -52,6 +53,10 @@ const App = (props) => {
           <Switch>
             <Route path="/movies/edit/:id">
             <EditMovieForm {...props} movies = {movies} setMovies = {setMovies}/>
+            </Route>
+
+            <Route path="/movies/add">
+            <AddMovieForm {...props} movies = {movies} setMovies = {setMovies}/>
             </Route>
 
             <Route path="/movies/:id">
